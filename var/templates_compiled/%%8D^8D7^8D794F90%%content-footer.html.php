f37aa049a406568b890ffb90c0116d0be8320395
Paulo Patto <paulopatto@gmail.com>
2013-10-16 19:04:21 -0300

    [RELEASE CANDIDATE] Primeira versão funcionadno em produção. \o/

---
+++ var/templates_compiled/%%8D^8D7^8D794F90%%content-footer.html.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.18, created on 2013-10-16 17:36:46
+         compiled from layout/content-footer.html */ ?>
+<?php if ($this->_tpl_vars['showContentFrame']): ?>
+</div>
+<?php endif; ?>
---
+++ var/templates_compiled/%%F4^F43^F4358000%%tracker.html.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.18, created on 2013-10-16 17:36:46
+         compiled from tracker.html */ ?>
+<iframe id="tracker-frame" src="<?php echo $this->_tpl_vars['marketPcHost']; ?>
+/market/tracker/<?php if ($this->_tpl_vars['isUpgrade']): ?>upgrade<?php else: ?>install<?php endif; ?>-<?php echo $this->_tpl_vars['step']; ?>
+" width="0" height="0" frameborder="0" marginwidth="0" marginheight="0"></iframe>
